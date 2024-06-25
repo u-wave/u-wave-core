d88bd22d0f79a1b34f2a61bd669afb9ed38de78f
Renée Kooi <renee@kooi.me>
2024-06-25 12:39:41 +0200

    enable import/typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -531,6 +531,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:node/recommended',
     'plugin:@typescript-eslint/recommended',
   ],
