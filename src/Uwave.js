8b2ae37e61d74be7d79d1203f04e06b983260ea7
Renée Kooi <renee@kooi.me>
2024-08-24 18:50:54 +0200

    Remove mongodb connection code

--- src/Uwave.js
+++ src/Uwave.js
@@ -1,7 +1,6 @@
 import EventEmitter from 'node:events';
-import { promisify, inspect } from 'node:util';
+import { promisify } from 'node:util';
 import pg from 'pg';
-import mongoose from 'mongoose';
 import Redis from 'ioredis';
 import avvio from 'avvio';
 import { pino } from 'pino';
@@ -10,7 +9,6 @@ import httpApi, { errorHandling } from './HttpApi.js';
 import SocketServer from './SocketServer.js';
 import { Source } from './Source.js';
 import { i18n } from './locale.js';
-import models from './models/index.js';
 import configStore from './plugins/configStore.js';
 import booth from './plugins/booth.js';
 import chat from './plugins/chat.js';
@@ -24,7 +22,6 @@ import waitlist from './plugins/waitlist.js';
 import passport from './plugins/passport.js';
 import migrations from './plugins/migrations.js';
 
-const DEFAULT_MONGO_URL = 'mongodb://localhost:27017/uwave';
 const DEFAULT_REDIS_URL = 'redis://localhost:6379';
 
 class UwCamelCasePlugin extends CamelCasePlugin {
@@ -50,7 +47,6 @@ class UwCamelCasePlugin extends CamelCasePlugin {
  * >} RedisOptions
  * @typedef {{
  *   port?: number,
- *   mongo?: string,
  *   redis?: string | RedisOptions,
  *   logger?: import('pino').LoggerOptions,
  * } & import('./HttpApi.js').HttpApiOptions} Options
@@ -67,10 +63,6 @@ class UwaveServer extends EventEmitter {
   /** @type {import('express').Application} */
   // @ts-expect-error TS2564 Definitely assigned in a plugin
   express;
-
-  /** @type {import('./models/index.js').Models} */
-  // @ts-expect-error TS2564 Definitely assigned in a plugin
-  models;
 
   /** @type {import('./plugins/acl.js').Acl} */
   // @ts-expect-error TS2564 Definitely assigned in a plugin
@@ -136,9 +128,6 @@ class UwaveServer extends EventEmitter {
    */
   #sources = new Map();
 
-  /** @type {import('pino').Logger} */
-  #mongoLogger;
-
   /**
    * @param {Options} options
    */
@@ -154,7 +143,6 @@ class UwaveServer extends EventEmitter {
     this.locale = i18n.cloneInstance();
 
     this.options = {
-      mongo: DEFAULT_MONGO_URL,
       redis: DEFAULT_REDIS_URL,
       ...options,
     };
@@ -178,8 +166,6 @@ class UwaveServer extends EventEmitter {
       ],
       log: ['query', 'error'],
     });
-    this.mongo = mongoose.createConnection(this.options.mongo);
-    mongoose.set('debug', true);
 
     if (typeof options.redis === 'string') {
       this.redis = new Redis(options.redis, { lazyConnect: true });
@@ -187,23 +173,10 @@ class UwaveServer extends EventEmitter {
       this.redis = new Redis({ ...options.redis, lazyConnect: true });
     }
 
-    this.#mongoLogger = this.logger.child({ ns: 'uwave:mongo' });
-
     this.configureRedis();
-    this.configureMongoose();
 
-    boot.onClose(() => Promise.all([
-      this.redis.quit(),
-      this.mongo.close(),
-    ]));
+    boot.onClose(() => this.redis.quit());
 
-    // Wait for the connections to be set up.
-    boot.use(async () => {
-      this.#mongoLogger.debug('waiting for mongodb...');
-      await this.mongo.asPromise();
-    });
-
-    boot.use(models);
     boot.use(migrations);
     boot.use(configStore);
 
@@ -311,31 +284,6 @@ class UwaveServer extends EventEmitter {
     this.redis.on('connect', () => {
       log.info('connected');
       this.emit('redisConnect');
-    });
-  }
-
-  /**
-   * @private
-   */
-  configureMongoose() {
-    this.mongo.on('error', (error) => {
-      this.#mongoLogger.error(error);
-      this.emit('mongoError', error);
-    });
-
-    this.mongo.on('reconnected', () => {
-      this.#mongoLogger.info('reconnected');
-      this.emit('mongoReconnect');
-    });
-
-    this.mongo.on('disconnected', () => {
-      this.#mongoLogger.info('disconnected');
-      this.emit('mongoDisconnect');
-    });
-
-    this.mongo.on('connected', () => {
-      this.#mongoLogger.info('connected');
-      this.emit('mongoConnect');
     });
   }
 
