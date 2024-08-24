c77f66cb34904490f28a91fd040fadf9054a7b45
Renée Kooi <renee@kooi.me>
2024-08-24 19:48:42 +0200

    Make migrations work in sql

--- src/plugins/migrations.js
+++ src/plugins/migrations.js
@@ -6,46 +6,6 @@ import { Umzug } from 'umzug';
  * @typedef {import('../Uwave.js').default} Uwave
  */
 
-/**
- * Custom MongoDBStorage based on Mongoose and with timestamps.
- */
-const mongooseStorage = {
-  /**
-   * @param {import('umzug').MigrationParams<Uwave>} params
-   */
-  async logMigration({ name, context: uw }) {
-    const { Migration } = uw.models;
-
-    await Migration.create({
-      migrationName: name,
-    });
-  },
-
-  /**
-   * @param {import('umzug').MigrationParams<Uwave>} params
-   */
-  async unlogMigration({ name, context: uw }) {
-    const { Migration } = uw.models;
-
-    await Migration.deleteOne({
-      migrationName: name,
-    });
-  },
-
-  /**
-   * @param {{ context: Uwave }} params
-   */
-  async executed({ context: uw }) {
-    const { Migration } = uw.models;
-
-    /** @type {{ migrationName: string }[]} */
-    const documents = await Migration.find({})
-      .select({ migrationName: 1 })
-      .lean();
-    return documents.map((doc) => doc.migrationName);
-  },
-};
-
 const kyselyStorage = {
   /**
    * @param {import('umzug').MigrationParams<Uwave>} params
@@ -80,25 +40,6 @@ const kyselyStorage = {
   },
 };
 
-const storage = {
-  logMigration: kyselyStorage.logMigration,
-  /**
-   * @param {import('umzug').MigrationParams<Uwave>} params
-   */
-  async unlogMigration(params) {
-    await kyselyStorage.unlogMigration(params);
-    await mongooseStorage.unlogMigration(params);
-  },
-  /**
-   * @param {{ context: Uwave }} params
-   */
-  async executed(params) {
-    const kyselyRows = await kyselyStorage.executed(params);
-    const mongooseRows = await mongooseStorage.executed(params);
-    return [...mongooseRows, ...kyselyRows];
-  },
-};
-
 /**
  * @typedef {import('umzug').InputMigrations<Uwave>} MigrateOptions
  * @typedef {(opts: MigrateOptions) => Promise<void>} Migrate
@@ -108,14 +49,20 @@ const storage = {
  * @param {Uwave} uw
  */
 async function migrationsPlugin(uw) {
+  const { schema } = uw.db;
   const redLock = new RedLock([uw.redis]);
+
+  schema.createTable('migrations')
+    .ifNotExists()
+    .addColumn('name', 'text', (col) => col.notNull().unique())
+    .execute();
 
   /** @type {Migrate} */
   async function migrate(migrations) {
     const migrator = new Umzug({
       migrations,
       context: uw,
-      storage,
+      storage: kyselyStorage,
       logger: uw.logger.child({ ns: 'uwave:migrations' }),
     });
 
