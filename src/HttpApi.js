6d4b13d9613423ff7edc9602e6322b35edd1fed1
Renée Kooi <renee@kooi.me>
2024-10-05 16:31:37 +0200

    enable nested qs parsing

--- src/HttpApi.js
+++ src/HttpApi.js
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import helmet from 'helmet';
+import qs from 'qs';
 import { pinoHttp } from 'pino-http';
 
 // routes
@@ -131,6 +132,8 @@ async function httpApi(uw, options) {
     .use('/users', users());
 
   uw.express = express();
+  uw.express.set('query parser', /** @param {string} str */ (str) => qs.parse(str, { depth: 1 }));
+
   uw.server = http.createServer(uw.express);
   if (options.helmet !== false) {
     uw.express.use(helmet({
