5099496ff7a0abb8132aede06a4d148b51a8de5f
Renée <renee@kooi.me>
2024-11-26 19:37:43 +0000

    Isolate MongoDB references to its migration (#673)
    * Move mongoose models into SQLite migration
    
    * Remove references to mongo, except in migration

--- bin/u-wave-core.js
+++ bin/u-wave-core.js
@@ -42,10 +42,9 @@ const envSchema = {
       format: 'uri',
       default: 'redis://localhost:6379',
     },
-    MONGODB_URL: {
+    SQLITE_PATH: {
       type: 'string',
-      format: 'uri',
-      default: 'mongodb://localhost:27017/uwave',
+      default: 'uwave.sqlite',
     },
     SECRET: {
       type: 'string',
@@ -74,10 +73,10 @@ if (argv.h || argv.help || !validConfig) {
   console.log('    A secret key used for encrypting passwords. Must be a 64-character hexadecimal string (= 256 bits).');
   console.log('  PORT');
   console.log('    Port to listen on. Defaults to 6042.');
+  console.log('  SQLITE_PATH');
+  console.log('    Path to the database file. Defaults to uwave.sqlite.');
   console.log('  REDIS_URL');
   console.log('    URL of the Redis instance to connect to. Defaults to redis://localhost:6379/.');
-  console.log('  MONGODB_URL');
-  console.log('    URL of the MongoDB database to use. Defaults to mongodb://localhost:27017/uwave.');
   console.log('  YOUTUBE_API_KEY [optional]');
   console.log('    Your YouTube Data API key.');
   console.log();
@@ -107,12 +106,8 @@ const secret = Buffer.from(config.SECRET, 'hex');
 const uw = uwave({
   port,
   redis: config.REDIS_URL,
-  mongo: config.MONGODB_URL,
+  sqlite: config.SQLITE_PATH,
   secret,
-});
-
-uw.on('mongoError', (err) => {
-  throw explain(err, 'Could not connect to MongoDB. Is it installed and running?');
 });
 
 uw.on('redisError', (err) => {
